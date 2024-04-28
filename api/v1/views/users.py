115f70b8db3ecc17c845a1f1d8c142a561cb3a66
Jesufemi <oladapofemmy@gmail.com>
2024-04-28 19:24:31 +0100

    fix9

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -11,7 +11,7 @@ from api.v1.views import app_views
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def get_users():
     """ Retrieves the list of all User objects"""
-    users = storage.all(User)
+    users = storage.all(User).values()
     list = []
     for u in users:
         list.append(u.to_dict())
